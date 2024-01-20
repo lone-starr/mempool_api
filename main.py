53bfcac3b965d5dc51c3eb1415a3e3718bcf08e3
Chris Melvin <chris@lonestarr.xyz>
2024-01-20 16:17:24 -0500

    throwing everything at it, i'll reduce later

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Response, status
+from fastapi.responses import HTMLResponse, RedirectResponse
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from urllib.parse import quote
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import secrets
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
 
 app = FastAPI()
 

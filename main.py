5754655112d19b43e7c06be695fc3cb3e7d7cfa5
Chris Melvin <chris@lonestarr.xyz>
2024-01-21 14:04:12 -0500

    Refactor to prepare saving more values. Only returning blockheight because main purpose of service is to write to persistance layer

--- main.py
+++ main.py
@@ -15,20 +15,24 @@ MONGO_URI = os.environ.get('MONGO_URI')
 
 @app.get("/getblocktip")
 async def getblocktip():
-    url = "https://mempool.space/api/blocks/tip/height"
-    response = requests.get(url)
-    response_json = response.json()
-    save_blockheight(response_json)
-    return {response_json}
-
-
-def save_blockheight(blockheight):
+    bh_response = requests.get("https://mempool.space/api/blocks/tip/height")
+    blockheight = bh_response.json()
+    count = 0
+    vsize = 0
     data = {
         "height": blockheight,
+        "count": count,
+        "vsize": vsize,
         "ts": datetime.now()
     }
+    save_data(data)
+    return {blockheight}
+
+
+def save_data(data):
     db = get_db('blockheight')
     db.insert_one(data)
+    return data
 
 
 def get_db(db_name):

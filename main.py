e01031c81997761d48e65349f14639f60282d223
Chris Melvin <chris@lonestarr.xyz>
2024-01-21 13:12:56 -0500

    fixed environmental issues in dev

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from fastapi import FastAPI
 from fastapi import FastAPI, HTTPException, Response, status
 from fastapi.responses import HTMLResponse, RedirectResponse
-from pydantic import BaseModel, EmailStr
-from typing import Optional
-from urllib.parse import quote
-from dotenv import load_dotenv
 from datetime import datetime
 from pymongo import MongoClient
 import os
 import requests
+from dotenv import load_dotenv
 
-MONGO_URI = os.environ.get('MONGO_URI')
+load_dotenv(".env")
 
 app = FastAPI()
 
+MONGO_URI = os.environ.get('MONGO_URI')
 
 @app.get("/getblocktip")
 async def getblocktip():
